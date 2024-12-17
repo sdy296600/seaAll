9796cf4c50b7388d825d89c0bc8bb933c47b2cb8
duf7317 <duf7317@naver.com>
2024-12-17 10:55:22 +0900

    Update Form1.cs

--- CalculateForSea/CalculateForSea/Form1.cs
+++ CalculateForSea/CalculateForSea/Form1.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -1784,6 +1785,7 @@ namespace CalculateForSea
             catch (Exception)
             {
                 WriteLog("13호기 값 없음");
+                StreamWriter file = new StreamWriter(Directory.GetCurrentDirectory() + "\\")
             }
         }
 
