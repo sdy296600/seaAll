b035b900eccb4fdd7447040228145d6d715a6780
sdy296600 <86033965+sdy296600@users.noreply.github.com>
2024-12-18 12:37:04 +0900

    .

--- SEA_MES/CoFAS.NEW.MES.Core/Properties/Resources.Designer.cs
+++ SEA_MES/CoFAS.NEW.MES.Core/Properties/Resources.Designer.cs
@@ -709,5 +709,15 @@ namespace CoFAS.NEW.MES.Core.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
+        internal static System.Drawing.Bitmap 모니터링_화면__1_2 {
+            get {
+                object obj = ResourceManager.GetObject("모니터링 화면 (1)2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
