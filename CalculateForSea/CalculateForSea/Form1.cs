4696c0d7fe28f3fd08aeca96fda4d91d30450247
duf7317 <duf7317@naver.com>
2024-12-19 16:53:07 +0900

    Update Form1.cs

--- CalculateForSea/CalculateForSea/Form1.cs
+++ CalculateForSea/CalculateForSea/Form1.cs
@@ -1390,7 +1390,7 @@ namespace CalculateForSea
 
                             nowPordCnt = intwork_okcnt
                                 + intwork_errcnt;
-                            if (nowtotalcnt <= models[i].Totalcnt)
+                            if (models[i].Totalcnt != -1  && nowtotalcnt <= models[i].Totalcnt)
                             {
                                 // 중복 또는 비정상 데이터로 판단하여 처리 스킵
                                 models[i].is_Running = false;
