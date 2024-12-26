897be809ffffa5e6a71fe4e6c11b1fb3bfc8d7f6
duf7317 <duf7317@naver.com>
2024-12-26 10:40:15 +0900

    커밋

--- DataMonitoringSystem/DataMonitoringSystem/DataMonitoringSystem/MainWindow.xaml.cs
+++ DataMonitoringSystem/DataMonitoringSystem/DataMonitoringSystem/MainWindow.xaml.cs
@@ -19,6 +19,8 @@ namespace DataMonitoringSystem
     {
         public static List<MainModel> mainModels;
         public static Dictionary<int,ElecModel> elecModels;
+        public static Dictionary<int, ElecModel2> elecModels2;
+
 
         public MainWindow()
         {
@@ -34,21 +36,25 @@ namespace DataMonitoringSystem
             // 다른 화면일 경우 Back 버튼 보이기
             BackButton.Visibility = Visibility.Collapsed;
 
-            mainModels = new List<MainModel>()
-            {
+            mainModels =
+            [
                 new MainModel(13),
                 new MainModel(21),
                 new MainModel(22),
                 new MainModel(23),
                 new MainModel(24),
                 new MainModel(25)
-            };
+            ];
 
             elecModels = new Dictionary<int, ElecModel>();
+            elecModels2 = new Dictionary<int, ElecModel2>();
+
             foreach (MainModel model in mainModels) 
             {
                 elecModels.Add(model.MachineNo,new ElecModel(model.MachineNo));
+                elecModels2.Add(model.MachineNo, new ElecModel2(model.MachineNo));
             }
+       
 
             MainView.ContentRendered += MainView_ContentRendered;
             All_Machine_View machine_View = new All_Machine_View(mainModels);
@@ -61,16 +67,18 @@ namespace DataMonitoringSystem
             if (MainView.Content is Page page)
             {
                 this.TitleContent.Content = page.Title;
-                
-                if (MainView.Content is All_Machine_View)
+
+                if (MainView.Content is iButtonProperty btnIntface)
                 {
-                    // 다른 화면일 경우 Back 버튼 보이기
-                    BackButton.Visibility = Visibility.Collapsed;
+                    // ButtonPropertyInterface로 성공적으로 캐스팅된 경우
+                    BackButton.Visibility = btnIntface.BackVisibilty;
+                    NextButton.Visibility = btnIntface.NextVisibilty;
                 }
                 else
                 {
-                    // 다른 화면일 경우 Back 버튼 보이기
-                    BackButton.Visibility = Visibility.Visible;
+                    // 기본값 설정 (예: 버튼 숨기기)
+                    BackButton.Visibility = Visibility.Collapsed;
+                    NextButton.Visibility = Visibility.Collapsed;
                 }
             }
 
@@ -80,15 +88,22 @@ namespace DataMonitoringSystem
         private void BackButton_MouseDown(object sender, MouseButtonEventArgs e)
         {
             // 뒤로 가기 동작
-            if (MainView.CanGoBack)
-            {
-                MainView.GoBack();
-            }
+            MainView.Navigate(new All_Machine_View(mainModels));
         }
 
         private void NextButton_MouseDown(object sender, MouseButtonEventArgs e)
         {
+            if (MainView.Content is DetailView1 model)
+            {
+                int machine_no = model.machine_no;
+                MainView.Navigate(new DetailView2(elecModels2[machine_no], machine_no));
+            }
+            else if(MainView.Content is DetailView2 model2) 
+            {
 
+                int machine_no = model2.machine_no;
+                MainView.Navigate(new DetailView1(elecModels[machine_no], machine_no));
+            }
         }
     }
 }
